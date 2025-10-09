fc80126139d41332b2e7d7f69096076f75f37395
czyt <czytcn@gmail.com>
2025-10-09 17:57:15 +0800

    allow use auth key for login

--- dist/run.sh
+++ dist/run.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 mkdir -p -m 0755 /tmp/tailscale
-TAILSCALED_PORT="41641"
 rm -rf /tmp/tailscale/tailscaled.pid
-tailscaled --port=$TAILSCALED_PORT --state=/var/lib/tailscale/tailscaled.state --socket=/run/tailscale/tailscaled.sock  2> /dev/null &
+
+# start tailscaled
+tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/run/tailscale/tailscaled.sock 2> /dev/null &
 echo $! > /tmp/tailscale/tailscaled.pid
 sleep 5
+
 if [ -e /tmp/tailscale/tailscaled.pid ]; then
     PID=$(cat /tmp/tailscale/tailscaled.pid)
-    echo "tailscaled (pid:$PID) is up ,apply options $TS_EXTRA_ARGS"
-    tailscale set $TS_EXTRA_ARGS
-    echo "Web client enabled..."
-    tailscale web --listen=0.0.0.0:59527
+    echo "tailscaled (pid:$PID) is up"
+    # if auth key provided, join the network
+    if [ -n "$TS_AUTHKEY" ]; then
+        echo "Authenticating with auth key..."
+        tailscale up --authkey=$TS_AUTHKEY ${TS_HOSTNAME:+--hostname=$TS_HOSTNAME} --accept-dns=false
+    fi
+
+    # apply other configurations
+    if [ -n "$TS_EXTRA_ARGS" ]; then
+        echo "Applying options: $TS_EXTRA_ARGS"
+        tailscale set $TS_EXTRA_ARGS
+    fi
+
+    # star web ui
+    echo "Web client enabled, serving at https://$LAZYCAT_APP_DOMAIN..."
+    tailscale web --listen=0.0.0.0:59527 --origin=https://$LAZYCAT_APP_DOMAIN
     echo "tailscale is ready"
 fi
